def7cb3b99ce6da6c5a0ac5a225c4cca413e57d9
Promise <tshegofatsopromise362@gmail.com>
2024-01-06 14:16:35 +0200

    06/01/2024

--- app.js
+++ app.js
@@ -10,40 +10,52 @@ app.listen(port, () => {
 
 const nodemailer = require('nodemailer');
 
+app.post('/api/forma', (req,res) =>{
+	let data = req.body
+	let mailTransporter = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+			user: 'promisetshegofatsom@gmail.com',
+			pass: 'zlfd xvcs sagv vxai'
+		}
+	});
 
 
-let mailTransporter = nodemailer.createTransport({
-	service: 'gmail',
-	auth: {
-		user: 'promisetshegofatsom@gmail.com',
-		pass: 'zlfd xvcs sagv vxai'
-	}
-});
 
 let mailDetails = {
-	from: 'promisetshegofatsom@gmail.com',
+	from: data.email,
 	to: 'tshegofatsopromise362@gmail.com',
-	subject: 'Registration for a learner from Tshepi',
+	subject: `Registration for a learner from ${data.name}`,
 	text: "testing nodemailer if it's working",
     html: `
     <h3>Registration Information</h3>
     <ul>
-    <li> Full Names: Tshepi</li>
-    <li> Full Surname: Mohale</li>
-    <li> Email: tshepi@gmail.com</li>
+    <li> Full Names: ${data.name}</li>
+    <li> Full Surname: ${data.surname}</li>
+    <li> Email: ${data.email}</li>
 
     
     </ul>
-    
-    
     `
+
+	
 };
 
-mailTransporter.sendMail(mailDetails, function(err, data) {
-	if(err) {
-		console.log('Error Occurs');
-        console.log(err);
-	} else {
-		console.log('Email sent successfully');
-	}
-});
+mailTransporter.sendMail(mailDetails, function (err, data) {
+    if (err) {
+      console.log('Error Occurs');
+      console.log(err);
+      res.status(500).send('Error Occurs'); // Send an error response
+    } else {
+      console.log('Email sent successfully');
+      res.status(200).send('Email sent successfully'); // Send a success response
+    }
+    mailTransporter.close(); // Close the transporter
+  });
+
+})
+
+
+
+
+
