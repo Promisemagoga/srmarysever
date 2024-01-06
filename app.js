b1301b5ea2801be4a118ab34a9e279b17745bc31
Promise <tshegofatsopromise362@gmail.com>
2024-01-06 12:49:04 +0200

    06/01/2024

--- app.js
+++ app.js
@@ -4,56 +4,46 @@ const port = 30002
 
 
 app.listen(port, () => {
-    console.log(`nodemailerProject is listening at http://localhost:${port}`)
+  console.log(`nodemailerProject is listening at http://localhost:${port}`)
 })
 
 
 const nodemailer = require('nodemailer');
 
-app.post('/form', (req, res) => {
-    let data = req.body
-
-    let mailTransporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'promisetshegofatsom@gmail.com',
-            pass: 'zlfd xvcs sagv vxai'
-        }
-    });
 
 
-    let mailDetails = {
-        from: data.email,
-        to: 'tshegofatsopromise362@gmail.com',
-        subject: `Registration for a learner from ${data.name}`,
-        html: `
-                <h3>Registration Information</h3>
-         <ul>
-                <li> Full Names: ${data.fullNames}</li>
-                <li> Surname: ${data.surname}</li>
-                <li> Email: ${data.email}</li>
+let mailTransporter = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+		user: 'promisetshegofatsom@gmail.com',
+		pass: 'zlfd xvcs sagv vxai'
+	}
+});
+
+let mailDetails = {
+	from: 'promisetshegofatsom@gmail.com',
+	to: 'tshegofatsopromise362@gmail.com',
+	subject: 'Registration for a learner from Tshepi',
+	text: "testing nodemailer if it's working",
+    html: `
+    <h3>Registration Information</h3>
+    <ul>
+    <li> Full Names: Tshepi</li>
+    <li> Full Surname: Mohale</li>
+    <li> Email: tshepi@gmail.com</li>
 
     
-        </ul>
+    </ul>
     
     
     `
-    };
+};
 
-    mailTransporter.sendMail(mailDetails, function (err, data) {
-        if (err) {
-            console.log('Error Occurs');
-            console.log(err);
-        } else {
-            console.log('Email sent successfully');
-        }
-    });
-
-
-
-})
-
-
-
-
-
+mailTransporter.sendMail(mailDetails, function(err, data) {
+	if(err) {
+		console.log('Error Occurs');
+        console.log(err);
+	} else {
+		console.log('Email sent successfully');
+	}
+});
